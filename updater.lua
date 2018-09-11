c9377d8cd91446de6cd5e7b9ac0153582a29abf4
Christian Groothoff <ruffo324@gmail.com>
2018-09-11 22:03:11 +0200

    Adding ui to utils

--- updater.lua
+++ updater.lua
@@ -7,6 +7,7 @@ shell.run("delete", "/Utils/StringUtils")
 shell.run("delete", "/Utils/updater")
 shell.run("delete", "/Utils/CodingUtils")
 shell.run("delete", "/Utils/Utils")
+shell.run("delete", "/Utils/Ui")
 
 -- Runn installer to get all new files.
 shell.run("/Utils/installer")
