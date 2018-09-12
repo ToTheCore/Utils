93c25d74683d3e7899d6adfc3a40e771830818ee
miscellaniuz <43073620+miscellaniuz@users.noreply.github.com>
2018-09-12 20:14:29 +0200

    added num formatting

--- StringUtils.lua
+++ StringUtils.lua
@@ -13,3 +13,28 @@ function PadRight(str, length, chr)
   
   return str
 end
+
+-- Formats big numbers, e.g. 1234567 to 1.234.567 for better reading
+function numformat(number)
+    if string.len(number) < 4 then
+    elseif string.len(number) < 7 then
+      number = string.gsub(number, "^(-?%d+)(%d%d%d)", '%1.%2')
+    elseif string.len(number) < 10 then
+      number = string.gsub(number, "^(-?%d+)(%d%d%d)(%d%d%d)", '%1.%2.%3')
+    elseif string.len(number) < 13 then
+      number = string.gsub(number, "^(-?%d+)(%d%d%d)(%d%d%d)(%d%d%d)", '%1.%2.%3.%4')
+    elseif string.len(number) > 13 then
+      number = string.gsub(number, "^(-?%d+)(%d%d%d)(%d%d%d)(%d%d%d)(%d%d%d)", '%1.%2.%3.%4.%5')
+    end
+  return number
+end
+
+
+-- Formats big numbers, e.g. 1234 to 1,2k and 1234567 to 1,2m
+-- function numformatk(number)
+--    if string.len(number) < 4 then
+--    elseif string.len(number) < 7 then
+--      number = string.gsub(number, "^(-?%d+)(%d%d%d)", '%1.%2')
+--    elseif string.len(number) < 10 then
+--      number = string.gsub(number, "^(-?%d+)(%d%d%d)(%d%d%d)", '%1.%2.%3')
+--
